be3dc4ccc8c2fb00729cb15b021b7ec8cc32aaf7
Leonie Dong <leoniedong@hotmail.com>
2019-05-22 08:55:41 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,42 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  languages = {
+    :oo => {
+      :ruby => {
+        :type => "interpreted"
+      },
+      :javascript => {
+        :type => "interpreted"
+      },
+      :python => {
+        :type => "interpreted"
+      },
+      :java => {
+        :type => "compiled"
+      }
+    },
+    :functional => {
+      :clojure => {
+        :type => "compiled"
+      },
+      :erlang => {
+        :type => "compiled"
+      },
+      :scala => {
+        :type => "compiled"
+      },
+      :javascript => {
+        :type => "interpreted"
+      }
+
+    }
+  }
+
+  new_hash = {}
+  languages.each do |des, name|
+    new_hash < name
+    new_hash[:name] < des
+    end
+  new_hash
 end
